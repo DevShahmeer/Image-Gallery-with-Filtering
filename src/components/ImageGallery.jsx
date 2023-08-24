8e97bbb4942dfbccf0a2161a663e17881fdf031d
Devshahmeer <theshahmeerfsd@gmail.com>
2023-08-24 22:39:48 +0500

    Integrated Unsplash API for image fetching

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -7,6 +7,35 @@ const ImageGallery = () => {
   const [selectedCategory, setSelectedCategory] = useState('');
 
   useEffect(() => {
+    let apiUrl = 'https://api.unsplash.com/photos/';
+    const accessKey = 'uOR7-Vy1_edxDc3bKjyt1OofnqUwJnPPsnIYzk4xW6U'; // Replace with your actual Unsplash access key
+
+    if (selectedCategory) {
+      apiUrl = `https://api.unsplash.com/search/photos/`;
+    }
+
+    const fetchData = async (page) => {
+      try {
+        const response = await axios.get(apiUrl, {
+          params: {
+            client_id: accessKey,
+            query: selectedCategory,
+            page: page,
+            per_page: 20 // You can adjust the number of images per page
+          }
+        });
+  
+        if (selectedCategory) {
+          setImages(response.data.results);
+        } else {
+          setImages(response.data);
+        }
+      } catch (error) {
+        console.error('Error fetching images:', error);
+      }
+    };
+  
+    fetchData(1); // Fetch the first page of images
     
   }, []);
 
