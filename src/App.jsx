f252ceb476b1d66a47b791a2731ed9683405e183
Devshahmeer <theshahmeerfsd@gmail.com>
2023-08-24 22:31:58 +0500

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="App">
+        <header className="App-header">
+          <h1>Image Gallery with Unsplash API</h1>
+        </header>
+        <main>
+        </main>
+      </div>
+    </>
+  )
+}
+
+export default App
